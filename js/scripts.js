a4b025452af4dc36ac0d498cecd9247e836eccaf
Mohammed Fachihul Alam <fachirbl@gmail.com>
2025-06-04 18:24:56 +0600

    d

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,26 @@ $(document).ready(function () {
     time: 1000,
   });
 });
+
+$(document).ready(function () {
+  // Portfolio filter
+  $(".portfolio-filters a").click(function (e) {
+    e.preventDefault();
+
+    const filter = $(this).data("filter");
+
+    // Highlight active filter
+    $(".portfolio-filters a").removeClass("active");
+    $(this).addClass("active");
+
+    // Show/hide items based on category
+    $(".portfolio-item").each(function () {
+      const categories = $(this).data("category").split(" ");
+      if (filter === "all" || categories.includes(filter)) {
+        $(this).fadeIn(300);
+      } else {
+        $(this).fadeOut(300);
+      }
+    });
+  });
+});
