758a79cb1ca8765451bacd561adee9d8ee7ebb20
Mohammed Fachihul Alam <fachirbl@gmail.com>
2025-06-17 13:58:45 +0600

    f

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,6 @@ $(document).ready(function () {
     });
   });
 });
+// For logo Carousel
+let copy = document.querySelector(".logo-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
